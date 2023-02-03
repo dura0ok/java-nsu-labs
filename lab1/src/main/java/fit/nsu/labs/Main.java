a71ff74bc8abbb6242b69da86735106b121c752e
stepan-neretin7 <fenixrnd@mail.ru>
2023-02-03 13:55:39 +0700

    some little changes

--- lab1/src/main/java/fit/nsu/labs/Main.java
+++ lab1/src/main/java/fit/nsu/labs/Main.java
@@ -1,5 +1,6 @@
 package fit.nsu.labs;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,21 +8,27 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
-            InputStream input = System.in;
-            if (args.length > 1) {
-                throw new RuntimeException(
-                        "you can add only 1 file to read from or you can not choose file and use stdin"
-                );
-            }
-
-            if (args.length == 1) {
-                input = Files.newInputStream(Paths.get(args[0]));
-            }
+            InputStream input = selectInputStreamFromArgs(args);
 
             CalcEvaluator calc = new CalcEvaluator(input);
             calc.calculate();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static InputStream selectInputStreamFromArgs(String[] args) throws IOException {
+        InputStream input = System.in;
+
+        if (args.length > 1) {
+            throw new RuntimeException(
+                    "you can add only 1 file to read from or you can not choose file and use stdin"
+            );
+        }
+
+        if (args.length == 1) {
+            input = Files.newInputStream(Paths.get(args[0]));
+        }
+        return input;
     }
 }

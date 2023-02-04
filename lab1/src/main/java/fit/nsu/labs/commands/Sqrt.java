761edb1638e48a629ee86462a233f5a0a1312288
stepan-neretin7 <fenixrnd@mail.ru>
2023-02-04 13:14:41 +0700

    add SqrtCommandTest and some refactor

--- lab1/src/main/java/fit/nsu/labs/commands/Sqrt.java
+++ lab1/src/main/java/fit/nsu/labs/commands/Sqrt.java
@@ -1,6 +1,7 @@
 package fit.nsu.labs.commands;
 
 import fit.nsu.labs.Context;
+import fit.nsu.labs.exceptions.EmptyStack;
 import fit.nsu.labs.exceptions.InvalidCommandArgument;
 
 public class Sqrt extends Command {
@@ -16,13 +17,22 @@ public class Sqrt extends Command {
     }
 
     @Override
-    public void execute(Context context) throws InvalidCommandArgument {
+    public void execute(Context context) throws InvalidCommandArgument, EmptyStack {
 
         if (getArgs().length != 0) {
             throw new InvalidCommandArgument(getCommandName(), "size");
         }
 
-        var num = context.getStack().pop();
+        if (context.getStack().isEmpty()) {
+            throw new EmptyStack(getCommandName(), "what to print");
+        }
+
+        double num = context.getStack().pop();
+
+        if(num < 0){
+            throw new InvalidCommandArgument(getCommandName(), "num must be >= 0");
+        }
+
         context.getStack().push(Math.sqrt(num));
     }
 
---
+++ lab1/src/test/java/SqrtCommandTest.java
@@ -0,0 +1,60 @@
+import fit.nsu.labs.Context;
+import fit.nsu.labs.commands.Sqrt;
+import fit.nsu.labs.exceptions.EmptyStack;
+import fit.nsu.labs.exceptions.InvalidCommandArgument;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SqrtCommandTest {
+
+    @Test
+    void sqrtNaturalSquare() {
+        var stack = new ArrayDeque<Double>();
+        stack.push(16.0);
+        var addCommand = new Sqrt(new String[]{});
+        try {
+            addCommand.execute(new Context(stack, new HashMap<>()));
+            assertEquals(4, stack.pop());
+        } catch (Exception ignored) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void sqrtRealNumber() {
+        var stack = new ArrayDeque<Double>();
+        stack.push(2.25);
+        var addCommand = new Sqrt(new String[]{});
+        try {
+            addCommand.execute(new Context(stack, new HashMap<>()));
+            assertEquals(1.5, stack.pop());
+        } catch (Exception ignored) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void sqrtNegativeNumber() {
+        var stack = new ArrayDeque<Double>();
+        stack.push(-1.0);
+        var sqrtCommand = new Sqrt(new String[]{});
+        assertThrows(InvalidCommandArgument.class, () ->  sqrtCommand.execute(new Context(stack, new HashMap<>())));
+
+    }
+
+    @Test
+    void emptyStackError() {
+        var stack = new ArrayDeque<Double>();
+        var sqrtCommand = new Sqrt(new String[]{});
+        assertThrows(EmptyStack.class, () ->  sqrtCommand.execute(new Context(stack, new HashMap<>())));
+
+    }
+}

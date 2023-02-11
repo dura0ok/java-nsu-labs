598d9f43008ccb6a5b7a6a107bf94f153322432c
stepan-neretin7 <fenixrnd@mail.ru>
2023-02-11 15:57:06 +0700

    reformat code

--- lab1/src/main/java/fit/nsu/labs/Main.java
+++ lab1/src/main/java/fit/nsu/labs/Main.java
@@ -2,8 +2,6 @@ package fit.nsu.labs;
 
 import fit.nsu.labs.exceptions.CalcException;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -11,29 +9,33 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
-            InputStream input = selectInputStreamFromArgs(args);
-            CalcEvaluator calc = new CalcEvaluator(input);
-            calc.calculate();
+            if (args.length == 0) {
+                CalcEvaluator calc = new CalcEvaluator(System.in);
+                calc.calculate();
+                return;
+            }
+
+            if (args.length == 1) {
+                try (var input = Files.newInputStream(Paths.get(args[0]))) {
+                    CalcEvaluator calc = new CalcEvaluator(input);
+                    calc.calculate();
+                }
+
+                return;
+            }
+
+            throw new RuntimeException(
+                    "you can add only 1 file to read from or you can not choose file and use stdin"
+            );
+
         } catch (CalcException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+
     }
 
-    private static InputStream selectInputStreamFromArgs(String[] args) throws IOException, RuntimeException {
-        InputStream input = System.in;
 
-        if (args.length > 1) {
-            throw new RuntimeException(
-                    "you can add only 1 file to read from or you can not choose file and use stdin"
-            );
-        }
-
-        if (args.length == 1) {
-            input = Files.newInputStream(Paths.get(args[0]));
-        }
-        return input;
-    }
 }

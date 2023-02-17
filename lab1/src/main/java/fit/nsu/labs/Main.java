b9342a504491daee91516fe94593b4dc76c0d6fa
stepan-neretin7 <fenixrnd@mail.ru>
2023-02-17 13:16:24 +0700

    some refactor

--- lab1/src/main/java/fit/nsu/labs/Main.java
+++ lab1/src/main/java/fit/nsu/labs/Main.java
@@ -10,33 +10,29 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args.length == 0) {
-                CalcExecutor calc = new CalcExecutor(System.in);
-                calc.calculate();
-                return;
-            }
 
-            if (args.length == 1) {
-                try (var input = Files.newInputStream(Paths.get(args[0]))) {
-                    CalcExecutor calc = new CalcExecutor(input);
+            switch (args.length) {
+                case 0 -> {
+                    CalcExecutor calc = new CalcExecutor(System.in);
                     calc.calculate();
                 }
 
-                return;
+                case 1 -> {
+                    try (var input = Files.newInputStream(Paths.get(args[0]))) {
+                        CalcExecutor calc = new CalcExecutor(input);
+                        calc.calculate();
+                    }
+                }
+                default -> throw new BadNumberOfArguments(
+                        "You can add only 1 file to read from or stdin can be used by default"
+                );
             }
 
-            throw new BadNumberOfArguments(
-                    "You can add only 1 file to read from or stdin can be used by default"
-            );
 
         } catch (CalcException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
-
-
 }

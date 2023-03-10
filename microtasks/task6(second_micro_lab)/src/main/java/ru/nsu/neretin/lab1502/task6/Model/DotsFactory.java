ccb58b2db95e461af5924585a4da24962015455a
stepan-neretin7 <fenixrnd@mail.ru>
2023-03-10 14:12:58 +0700

    some renaming add fixes

--- microtasks/task6(second_micro_lab)/src/main/java/ru/nsu/neretin/lab1502/task6/Model/DotsFactory.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.neretin.lab1502.task6.Model;
-
-public class DotsFactory {
-    public static Plain create(int n){
-        return new Plain(n);
-    }
-}
--- microtasks/task7(first_micro_lab)/src/main/java/ru/nsu/neretin/lab0802/task7/Model/ArrayModifier.java
+++
@@ -1,5 +0,0 @@
-package ru.nsu.neretin.lab0802.task7.Model;
-
-public interface ArrayModifier {
-    int[] modify(int k, int[] a);
-}

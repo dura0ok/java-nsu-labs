551030a95be66bc7cdf41b1de7c9aeec22c59293
Dmitry Savenko <ds@dsavenko.com>
2023-04-09 20:52:23 +0700

    review

--- lab2/src/main/java/fit/nsu/labs/utils/RandomUtil.java
+++ lab2/src/main/java/fit/nsu/labs/utils/RandomUtil.java
@@ -3,6 +3,9 @@ package fit.nsu.labs.utils;
 import java.util.Random;
 
 public class RandomUtil {
+
+    // TODO: this class contains 1 method which is used in 1 other class. Stop doing utility classes when they're easy to avoid
+
     private static final Random randomService = new Random();
 
     private RandomUtil() {
